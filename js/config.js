f03925c5b094847baec87138b64ab52c3348c1b6
lsazama <104404872+lsazama@users.noreply.github.com>
2023-04-06 20:49:32 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'us-east-1_yWk9nAAQH',
+        userPoolClientId: '19obsdqnac8s8p2j3fib59iglj',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
